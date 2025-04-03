3b42ee84830aa0f62b20cede0cbe435e9be47456
senacelebi_tanbt <sena.celebi@tanbt.com>
2025-04-03 16:43:33 +0300

    ssa

--- dags/clean_data_dag.py
+++ dags/clean_data_dag.py
@@ -20,6 +20,7 @@ with DAG(
         task_id='run_clean_script_on_spark',
         ssh_conn_id='ssh_spark_client',
         command='python3 /dataops/scripts/clean_store_data.py',
+
     )
 
     run_clean_script
